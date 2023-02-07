f7dfa564bb26a2c01419afdee77f2e1a0201e1bb
Chris Stenke <chris@isfett.com>
2023-02-07 21:54:05 +0100

    Coding Challenge

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,106 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders headline', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headline = screen.getByText(/Planted Coding Challenge/i);
+  expect(headline).toBeInTheDocument();
+});
+
+test('renders searchfield', () => {
+  render(<App />);
+  const searchField = screen.getByRole("textbox");
+  expect(searchField).toBeInTheDocument();
+  expect(searchField).not.toHaveValue();
+});
+
+test('not renders hint when searchfield is empty', () => {
+  render(<App />);
+  const hint = screen.queryByText(/Ort nicht gefunden. Benutze bitte einen dieser Orte:/i);
+  expect(hint).not.toBeInTheDocument();
+});
+
+test('not renders hint when searchfield contains one empty space', () => {
+  render(<App />);
+  const hint = screen.queryByText(/Ort nicht gefunden. Benutze bitte einen dieser Orte:/i);
+  expect(hint).not.toBeInTheDocument();
+});
+
+test('renders hint when searchfield is not empty and location is not found', async () => {
+  const {container} = render(<App />);
+  const inputElement = container.querySelector("input[type='text']") as HTMLInputElement;
+  fireEvent.change(inputElement, {target: {value: "Foo"} });
+  expect(inputElement?.value).toEqual("Foo");
+  const hint = await screen.findByText(/Ort nicht gefunden. Benutze bitte einen dieser Orte: Neu-Ulm, Koblenz am Rhein, Ilmenau, Potsdam, Saarbrücken, Königstein im Taunus, Bad Kreuznach, Krebeck, Bergisch Gladbach, Wetzlar/i);
+  expect(hint).toBeInTheDocument();
+});
+
+test('not renders hint when searchfield is not empty and location is found', async () => {
+  const {container} = render(<App />);
+  const inputElement = container.querySelector("input[type='text']") as HTMLInputElement;
+  fireEvent.change(inputElement, {target: {value: "Ilmenau"} });
+  expect(inputElement?.value).toEqual("Ilmenau");
+  const hint = await screen.queryByText(/Ort nicht gefunden. Benutze bitte einen dieser Orte:/i);
+  expect(hint).not.toBeInTheDocument();
+});
+
+test('not renders hint when searchfield contains one empty space and location Bad Kreuznach is found', async () => {
+  const {container} = render(<App />);
+  const inputElement = container.querySelector("input[type='text']") as HTMLInputElement;
+  fireEvent.change(inputElement, {target: {value: " "} });
+  expect(inputElement?.value).toEqual(" ");
+  const hint = await screen.queryByText(/Ort nicht gefunden. Benutze bitte einen dieser Orte:/i);
+  expect(hint).not.toBeInTheDocument();
+});
+
+test('not render results when searchfield is empty', () => {
+  render(<App />);
+  const hint = screen.queryByText(/km entfernt/i);
+  expect(hint).not.toBeInTheDocument();
+});
+
+test('not render results when searchfield is not empty and location is not found', async () => {
+  const {container} = render(<App />);
+  const inputElement = container.querySelector("input[type='text']") as HTMLInputElement;
+  fireEvent.change(inputElement, {target: {value: "Foo"} });
+  expect(inputElement?.value).toEqual("Foo");
+  const result = await screen.queryByText(/Foo:/i);
+  expect(result).not.toBeInTheDocument();
+});
+
+test('not render results when searchfield contains one empty space and location Bad Kreuznach is found', async () => {
+  const {container} = render(<App />);
+  const inputElement = container.querySelector("input[type='text']") as HTMLInputElement;
+  fireEvent.change(inputElement, {target: {value: " "} });
+  expect(inputElement?.value).toEqual(" ");
+  const result = await screen.queryByText(/Bad Kreuznach:/i);
+  expect(result).not.toBeInTheDocument();
+});
+
+test('render results when searchfield is not empty and location is found', async () => {
+  const {container} = render(<App />);
+  const inputElement = container.querySelector("input[type='text']") as HTMLInputElement;
+  fireEvent.change(inputElement, {target: {value: "Ilmenau"} });
+  expect(inputElement?.value).toEqual("Ilmenau");
+
+  const result = await screen.findByText(/Ilmenau/i);
+  expect(result).toBeInTheDocument();
+
+  const projectName1 = await screen.findByText(/Bäume pflanzen im Sauerland/i);
+  expect(projectName1).toBeInTheDocument();
+  const distance1 = await screen.findByText(/191 km entfernt/i);
+  expect(distance1).toBeInTheDocument();
+  
+  const projectName2 = await screen.findByText(/Wella Firmenwald/i);
+  expect(projectName2).toBeInTheDocument();
+  const distance2 = await screen.findByText(/202 km entfernt/i);
+  expect(distance2).toBeInTheDocument();
+  
+  const projectName3 = await screen.findByText(/Pflanzung Hohe Wurzel \(Bereich A\)/i);
+  expect(projectName3).toBeInTheDocument();
+  const distance3 = await screen.findByText(/206 km entfernt/i);
+  expect(distance3).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
+import SearchField from './components/SearchField';
+import Project from './components/Project';
+import useDebounce from './hooks/useDebounce';
+
+import { findLocation, getAllLocationNames } from './services/LocationService';
+import { LocationType } from './types/Location';
+import { ProjectWithDistanceType } from './types/Project';
+import { findNumberOfProjectsByDistanceAsc } from './services/ProjectService';
+
 function App() {
+  const [searchInput, setSearchInput] = useState("");
+  const debouncedSearchInput = useDebounce(searchInput, 500);
+
+  const [searchLocation, setSearchLocation] = useState("");
+  const [hint, setHint] = useState("");
+
+  const numberOfProjectsToRender = 3;
+  const [projects, setProjects] = useState<ProjectWithDistanceType[]>();
+
+  useEffect(() => {
+    if (debouncedSearchInput.trim().length > 1) { // prevent empty space to be Bad Kreuznach or - to be Neu-Ulm
+      const tempSearchLocation: LocationType = findLocation(debouncedSearchInput);
+      if (!tempSearchLocation) {
+        setProjects(undefined);
+        setSearchLocation("");
+        setHint("Ort nicht gefunden. Benutze bitte einen dieser Orte: " + getAllLocationNames().join(", "));
+        return;
+      } 
+
+      setSearchLocation(tempSearchLocation.name);
+      setHint("");
+      
+      const projectsSortedByDistance: ProjectWithDistanceType[] = findNumberOfProjectsByDistanceAsc(numberOfProjectsToRender, tempSearchLocation.latitude, tempSearchLocation.longitude);
+      setProjects(projectsSortedByDistance);
+    }
+  }, [ debouncedSearchInput ])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        Planted Coding Challenge
+      </header> 
+      
+      <SearchField value={searchInput} onChange={setSearchInput} />
+
+      {"" !== hint && <div className="Search-Hint">{hint}</div>}
+
+      {"" !== searchLocation && <div className="Search-Location">{searchLocation}</div>} 
+      {undefined !== projects && projects.map((project: ProjectWithDistanceType) => (<Project key={project.id} {...project}/>))}
+      
     </div>
   );
 }
